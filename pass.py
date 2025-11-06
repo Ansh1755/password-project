5a554c90dd4bc4578055c4eb38f9378351edb8aa
ANSH TIWARI <anshbrahman1727@gmail.com>
2025-11-06 16:23:33 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,27 @@
+# Password Strength Checker
+
+password = input("Enter your password: ")
+
+# Conditions
+has_upper = False
+has_lower = False
+has_digit = False
+has_symbol = False
+
+symbols = "!@#$%^&*()-_=+[{]};:'\,<.>/?`~"
+
+for char in password:
+    if char.isupper():
+        has_upper = True
+    elif char.islower():
+        has_lower = True
+    elif char.isdigit():
+        has_digit = True
+    elif char in symbols:
+        has_symbol = True
+
+# Checking strength
+if len(password) >= 8 and has_upper and has_lower and has_digit and has_symbol:
+    print("Strong Password ")
+else:
+    print("Weak Password ")
